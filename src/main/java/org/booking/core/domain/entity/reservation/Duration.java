b9a36f43088fb1b52c16bbadb43bc868d46e3b21
Sergey <sergeipopof178@gmail.com>
2024-02-02 11:16:29 +0700

    ISSUE-42 executed new change sets

--- src/main/java/org/booking/core/domain/entity/reservation/Duration.java
+++ src/main/java/org/booking/core/domain/entity/reservation/Duration.java
@@ -1,11 +1,8 @@
 package org.booking.core.domain.entity.reservation;
 
-import lombok.ToString;
-
 import javax.persistence.Embeddable;
 import java.time.LocalDateTime;
 
-@ToString
 @Embeddable
 public class Duration {
 

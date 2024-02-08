3bf0bbbc252c971fb07391dd873fac7c572a26c2
Sergey <sergeipopof178@gmail.com>
2024-02-08 10:57:05 +0800

    ISSUE-42 corrected api

--- src/main/java/org/booking/core/api/AppointmentApi.java
+++ src/main/java/org/booking/core/api/AppointmentApi.java
@@ -2,9 +2,8 @@ package org.booking.core.api;
 
 
 import org.booking.core.domain.dto.ReservationDto;
-import org.booking.core.domain.request.AppointmentRequest;
-import org.booking.core.service.appointment.AppointmentSchedulerService;
 import org.booking.core.domain.entity.reservation.TimeSlot;
+import org.booking.core.service.appointment.AppointmentSchedulerService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +15,6 @@ import java.util.List;
 @RequestMapping(path = "/api/appointments", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class AppointmentApi {
 
-
     private final AppointmentSchedulerService appointmentSchedulerService;
 
     public AppointmentApi(AppointmentSchedulerService appointmentSchedulerService) {
@@ -24,10 +22,12 @@ public class AppointmentApi {
     }
 
     @GetMapping("/find/available-time-slots")
-    public ResponseEntity<List<TimeSlot>> findAvailableTimeSlots(@RequestParam AppointmentRequest request) {
-        List<TimeSlot> availableSlots = appointmentSchedulerService.findAvailableSlots(request.getBusinessId(),
-                request.getBusinessServiceId(),
-                LocalDate.ofEpochDay(request.getDay()));
+    public ResponseEntity<List<TimeSlot>> findAvailableTimeSlots(@RequestParam("businessId") Long businessId,
+                                                                 @RequestParam("businessServiceId") Long businessServiceId,
+                                                                 @RequestParam("day") Long day) {
+        List<TimeSlot> availableSlots = appointmentSchedulerService.findAvailableSlots(businessId,
+                businessServiceId,
+                LocalDate.ofEpochDay(day));
         return ResponseEntity.ok().body(availableSlots);
     }
 
@@ -38,14 +38,14 @@ public class AppointmentApi {
 
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<ReservationDto> update(@PathVariable("id")Long reservationId, @RequestBody ReservationDto dto) {
+    @PutMapping("/{reservationId}")
+    public ResponseEntity<ReservationDto> update(@PathVariable("reservationId")Long reservationId, @RequestBody ReservationDto dto) {
         ReservationDto modifiedReservation = appointmentSchedulerService.modifyReservation(reservationId, dto);
         return ResponseEntity.ok().body(modifiedReservation);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> delete(@PathVariable("id") Long reservationId) {
+    @DeleteMapping("/{reservationId}")
+    public ResponseEntity<Boolean> delete(@PathVariable("reservationId") Long reservationId) {
         return ResponseEntity.ok().body(appointmentSchedulerService.cancelReservation(reservationId));
     }
 

43b30cf1ddb874c85083023d61ac768ea169b4b2
Sergey <sergeipopof178@gmail.com>
2024-02-17 18:07:11 +0800

    ISSUE-42 fixed CustomerApiTestAssured test

--- src/main/java/org/booking/core/domain/dto/CustomerReservationHistoryDto.java
+++ src/main/java/org/booking/core/domain/dto/CustomerReservationHistoryDto.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class CustomerReservationHistoryDto {
     private Long id;
     private Set<ReservationDto> reservations;
-    private CustomerDto customer;
+    private Long customerId;
     private String eventType;
     private String eventTime;
     private String details;
